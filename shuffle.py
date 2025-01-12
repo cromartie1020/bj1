4092f1005b51c4f7d5cacdd197d5bbf8b16e99cd
cromartie1020 <hrc245@mail.com>
2025-01-11 22:53:14 -0500

    Add four function to Shuffle class.

--- shuffle.py
+++ shuffle.py
@@ -1,26 +1,40 @@
 import os,json
 from random import choice
 os.system('cls')
-
+four_cards=[]
 temp=[]
 cards=[card for card in range(1,53)]
+
 class Shuffle:
-        
-    def __init__(self,cards,temp):
+    '''
+      Requires cards a list including 1 to 52. An empty
+      temp list.
+      def __init__(self,cards,temp): 
+    '''    
+    def __init__(self,cards,temp, four_cards):
         self.cards = cards 
         self.temp=temp
+        self.four_cards=four_cards
+        
     def  select_card(self):    
         while len(cards)!= 0:
             selected=choice(cards)                              # Selects a random card.
             temp.append(selected)                               # Append that card to temp. 
             cards.remove(selected)                              # Now remove the selected card from cards. 
             
-        print(temp) 
+        #print(temp)
+        return temp 
          
-         
-         
-#new_card=Shuffle(temp)
-#temp=new_card.random_cards()
-#test = Shuffle(cards,temp)
-#temp = test.select_card()
-#print(temp)
+    def four(self):
+        hold = []
+        four_cards.append(temp.pop())
+        four_cards.append(temp.pop())
+        four_cards.append(temp.pop())
+        four_cards.append(temp.pop())
+        return four_cards     
+#-----------------Test the class.-----------------------------------------------------         
+
+
+test = Shuffle(cards,temp,four_cards)#temp = test.select_card()
+print(test.select_card())
+print(test.four())
